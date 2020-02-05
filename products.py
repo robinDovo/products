853defd2b700ec9042b8b337664db3df8248552a
robin Dovo <m104258979@gmail.com>
2020-02-05 15:56:04 +0800

    add1

--- products.py
+++ products.py
@@ -1,12 +1,16 @@
+import os
 products = []
-#讀檔案
-with open('products.csv', 'r' ,encoding='utf-8') as f:
-	for line in f:
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+	print('檔案存在！')
+	with open('products.csv', 'r' ,encoding='utf-8') as f:#讀取檔案
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
 
-
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱： ')
 	if name == 'q':
@@ -18,6 +22,7 @@ while True:
 	products.append([name, price])
 print(products)
 
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
